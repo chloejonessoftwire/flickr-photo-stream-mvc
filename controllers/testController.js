4cd3d61f9849cc36c888b7461c7c124feea78c79
Chloe Jones <Chloe.Jones@softwire.com>
2021-05-11 21:35:37 +0100

    cleaned up code

--- controllers/testController.js
+++ controllers/testController.js
@@ -2,16 +2,6 @@ const ImageClass = require('../models/Test');
 const { response } = require('express');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-// exports.getTestData = (req, res) => {
-// 	let data=[
-// 		new Test('other name', 15)
-// 	]
-		
-// 		res.render('testView', {
-// 			data : data,
-// })};
-
-
 exports.getFlickrData = (req, res) => {
 	
 	getFeedData()
@@ -42,54 +32,3 @@ exports.getFlickrData = (req, res) => {
         res.render('testView', {flickrArray: flickrArray})
     }   
 }
-//                 // If there is at least one stop point get the arrivals for that stop point
-//                 console.log(stopPointData.stopPoints)
-//                 if(stopPointData.stopPoints.length > 0) {
-//                     getArrivalsForStopPoint(stopPointData.stopPoints[0].id)
-//                 }
-                
-//             }
-//         }
-//         request.send()
-//     }
-
-// function getArrivalsForStopPoint(stopPoint) {
-// 	console.log('get arrivals  from stop points is runnning...')
-//     let url = `https://api.tfl.gov.uk/StopPoint/${stopPoint}/Arrivals`;
-//     var request = new XMLHttpRequest()
-//     request.open('GET', url, true)
-//     request.onreadystatechange = function () {
-//         if (request.readyState === 4) {
-//             let array = JSON.parse(request.responseText)
-//             sortData(array)
-//         }
-//     }
-//     request.send()
-// }
-
-// function sortData(buses) {
-// 	console.log('sort data is running')
-//     // Map the array of objects to a new array of objects with only the information we want
-//     let reducedBuses = buses.map(bus =>  { return { time: bus.timeToStation, line: bus.lineName, route: bus.towards }})
-
-//     // Sort the array of objects in order of their time value
-//     let sortedReducedBuses = reducedBuses.sort(function(a,b) {
-//         if (a.time < b.time) return -1
-//         else return 1
-//     })
-
-//     // Get the first 5 objects from the array
-//     let next5Buses = sortedReducedBuses.slice(0,5)
-//     console.log(next5Buses)
-//     // Print some information to the user to the console about each bus
-//     let busArray= next5Buses.map(bus=>{
-// 		return new BusClass (bus.time, bus.line, bus.route)
-//     });
-//     console.log(busArray)
-// 	res.render("postcodeView", {
-// 		postcode:userPostcode,
-// 		busArray: busArray
-// 	})
-// }
-
-// }
--- models/Test.js
+++ models/Test.js
@@ -1,15 +1,3 @@
-// class BusClass{
-// 	constructor(timeToStation, lineName, towards){
-// 		this.timeToStation=timeToStation
-// 		this.lineName=lineName;
-// 		this.towards=towards; 
-// 	}
-// 	showBuses(){
-// 		return 'The ' + this.lineName + ' line is ' +this.timeToStation +' seconds away and is heading towards '+ this.towards +'.'; 
-// 	}; 
-// }
-// module.exports=BusClass;  
-
 class ImageClass{
 	constructor( title, media, date_taken, author, tags, link, author_id){
 		this.title=title;
@@ -25,66 +13,5 @@ class ImageClass{
 		this.author_link='https://www.flickr.com/photos/'+author_id;
 	}
 	
-	// showImages(){
-	// 	return (
-	// 	<div>
-	// 		<img src={this.link} alt={this.title}/> 
-	// 		<h1> {this.title} </h1>
-	// 		<h2> by + {this.author}</h2>
-	// 		<h3> {this.date}</h3>
-	// 		<h3> Tags: {this.tags}.</h3>
-	// 	</div> )
-	// }
-	ShowImage(){
-		return this.link
-	}
-	ShowTitle(){
-		return this.title
-	}
-	ShowAuthor(){
-		return this.author
-	}
-	; 
 }
 module.exports=ImageClass;  
-/*
-class Test{
-	constructor(name, id) {
-		this.name = name;
-		this.id = id;
-	}
-
-	showTestData() {
-		return this.name + ", id: " + this.id; 
-	};
-
-	editName(newName) {
-		this.name = newName
-	}
-};
-
-module.exports = Test;
-/*
-class BusStopCode{
-	constructor(busStopCode, busStopName){
-		this.busStopCode= stopCode;
-		this.busStopName= stopName;
-	}
-	showBusStopCodeandName(){
-		return 'The nearest stop is:' + this.stopName + '(ATCO code:' +this.stopCode +')'; 
-	}; 
-}
-module.exports=BusStopCode; 
-*/
-
-	// class PostcodeClass{
-
-	// 	constructor(input){
-	// 		this.input=input
-	// 	}
-	// 	showPostCode(){
-	// 		return this.input +"working"
-	// 	}
-
-	// };
-	// module.exports=PostcodeClass
