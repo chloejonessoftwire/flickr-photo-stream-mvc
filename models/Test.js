33ca7e7686f507af826d86011cbf28a662174f6d
Chloe Jones <Chloe.Jones@softwire.com>
2021-05-12 14:46:06 +0100

    css added, grid responsive

--- models/Test.js
+++ models/Test.js
@@ -7,7 +7,7 @@ class ImageClass{
 		this.time=(this.date_time).toLocaleTimeString('en-GB', { hour: 'numeric',minute: 'numeric', hour12: true });
 		this.author_long= author.replace(/nobody@flickr.com/g,"");
 		this.author=(this.author_long).substring(3, this.author_long.length-2);
-		this.tags=tags;
+		this.tags=(tags.split(' ')).join(', #');
 		this.link=link;
 		this.author_id=author_id;
 		this.author_link='https://www.flickr.com/photos/'+author_id;
