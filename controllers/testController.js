ec72ac0065bfc6cb154a3bf634c8b3dd88705b0a
Chloe Jones <Chloe.Jones@softwire.com>
2021-05-12 16:34:21 +0100

    added hover states, started search bar functionality

--- controllers/testController.js
+++ controllers/testController.js
@@ -32,3 +32,35 @@ exports.getFlickrData = (req, res) => {
         res.render('testView', {flickrArray: flickrArray})
     }   
 }
+
+// exports.getFlickrData = (req, res) => {
+    
+//     let search = req.body.search
+// 	getFeedSearchData()
+
+//     function getFeedSearchData() {
+//         console.log('Getting Flickr Search Feed')
+
+//         let url = `https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&lang?en&safe_search=1&tags=${search}`;
+//         var request = new XMLHttpRequest()
+//         request.open('GET', url, true)
+//         request.onreadystatechange = function () {
+//             if (request.readyState === 4) {
+//                 let photoDataArray = (JSON.parse(request.responseText)).items
+//                 console.log(photoDataArray)
+//                 sortDataSearch(photoDataArray)
+//             }
+//         }
+//         request.send()
+//     }
+//     function sortDataSearch(photoDataArray){
+//         for (photo in photoDataArray) {
+//             console.log(photo)}
+
+//         let flickrArray = photoDataArray.map(photo => {
+//             return new ImageClass (photo.title, photo.media.m, photo.date_taken, photo.author, photo.tags, photo.link, photo.author_id)
+//         });
+//         console.log(flickrArray)
+//         res.render('testView', {flickrArray: flickrArray})
+//     }   
+// }
